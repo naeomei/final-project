9deafcead1f89feead8e56025f6b10b57a2ec940
LesserFlame <94986192+LesserFlame@users.noreply.github.com>
2021-11-29 10:34:38 -0700

    Add files via upload

---
+++ MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
